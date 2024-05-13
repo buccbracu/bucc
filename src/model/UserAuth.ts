b6f008d9750f5f1942f23511e9d324f0dfceb8d9
Eniamza <eniamza0@gmail.com>
2024-05-13 07:21:26 +0600

    Feat: Initial UserAuth Models

--- src/model/UserAuth.ts
+++ src/model/UserAuth.ts
@@ -1,3 +1,4 @@
+import { verify } from "crypto";
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
@@ -5,5 +6,38 @@ const { Schema } = mongoose;
 
 const UserAuthSchema = new mongoose.Schema({
     
-    userId: { type: Schema.Types.ObjectId, ref: 'UserInfo', required: true }
+    userId: { type: Schema.Types.ObjectId, ref: 'UserInfo', required: true, index: true },
+    userRoles:{
+        type: [String],
+        required: [true, 'Please provide a role'],
+        default: ['GM'] //enum validation needed here, UserRole ENUM(X_GB, CNM_EB, CR_EB, EM_EB, FN_EB, HR_EB, PR_EB, RND_EB, MEMBER)
+    },
+    passwordHash: { 
+        type: String, 
+        required: true },
+    verifyToken:{
+        type: String,
+        required: false
+    },
+    verifyTokenExpiry:{
+        type: Date,
+        required: false
+    },
+    resetToken:{
+        type: String,
+        required: false
+    },
+    resetTokenExpiry:{
+        type: Date,
+        required: false
+    },
+    isVerified:{
+        type: Boolean,
+        required: true,
+        default: false
+    }
 })
+
+const UserAuth = mongoose.models.UserAuth || mongoose.model('UserAuth', UserAuthSchema);
+
+export default UserAuth;
