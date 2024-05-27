5f9ce18bf4dd28a4c4a1a4755aacb1653c51e71f
abir <abir4dev@gmail.com>
2024-05-27 13:44:14 +0600

    db

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@/auth";
+import dbConnect from "@/lib/dbConnect";
 
 export const maxDuration = 60;
 export default async function Settings() {
+  await dbConnect();
   const session = await auth();
 
   return (
