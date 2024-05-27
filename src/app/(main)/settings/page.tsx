253dfa47a4e907653598dea776b64f6df3c8fdd0
abir <abir4dev@gmail.com>
2024-05-27 12:52:39 +0600

    general test

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,29 +1,13 @@
-"use client";
 import { auth } from "@/auth";
-import { useEffect, useState } from "react";
-import { Session } from "next-auth";
-export const maxDuration = 60;
-export default function Settings() {
-  const [session, setSession] = useState<Session | null>(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchSession() {
-      const sessionData = await auth();
-      setSession(sessionData);
-      setLoading(false);
-    }
-
-    fetchSession();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (!session?.user) return <p>No user session found</p>;
+export const maxDuration = 20;
+export default async function Settings() {
+  const session = await auth();
 
   return (
     <div>
       <p>{JSON.stringify(session)}</p>
-      <p>{session.user.userRole}</p>
+      <p>{session?.user.userRole}</p>
     </div>
   );
 }
