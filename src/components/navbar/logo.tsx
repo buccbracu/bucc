cbc278f44da11cd60b699468cbec66e5aae76236
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2024-05-27 14:40:44 +0600

    Fix: Hero section image changed and blur loading added

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -1,21 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import BUCCIcon from "/public/assets/bucc-icon.svg";
+import BUCCLogo from "/public/assets/bucc-logo.svg";
 export default function Logo() {
   return (
     <div>
       <Link href="/">
         <picture>
-          <source
-            media="(max-width: 768px)"
-            srcSet="/assets/bucc-icon.svg"
-            width={60}
-          />
-          <source media="(min-width: 769px)" srcSet="/assets/bucc-logo.svg" />
+          <source media="(max-width: 768px)" srcSet={BUCCIcon} width={60} />
+          <source media="(min-width: 769px)" srcSet={BUCCLogo} />
 
           <Image
             className="cursor-pointer dark:filter dark:invert dark:grayscale dark:brightness-0"
-            src="/assets/bucc-logo.svg"
+            src={BUCCLogo}
             alt="BUCC Logo"
             width={160}
             height={60}
