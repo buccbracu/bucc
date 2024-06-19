44435b0157fb585c0941fc53de673965c745d959
eniamza <eniamza0@gmail.com>
2024-06-20 01:01:36 +0600

    Feat: Add users/profile Endpoint, View only, Individual

--- src/app/api/(auth)/users/profile/route.ts
+++ src/app/api/(auth)/users/profile/route.ts
@@ -1,1 +1,25 @@
-//TODO: Show Profile Information
+import { auth } from "@/auth";
+import dbConnect from "@/lib/dbConnect";
+import MemberInfo from "@/model/MemberInfo";
+import { NextResponse } from "next/server";
+
+//This endpoint is view only for only individual members
+
+export async function GET() {
+    await dbConnect();
+    const user = await auth();
+
+    if (!user) {
+        return NextResponse.json({
+          message: "You are not authorized to view this page",
+        });
+      }
+
+      try {
+        const users = await MemberInfo.findOne({ _id: user.user.id }).lean;
+        return NextResponse.json({ user: users });
+      } catch (error) {
+        return NextResponse.json({ error: error });
+      }
+
+}
