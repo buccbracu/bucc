6a62889c9abfc058b0847887ca57e4170a9808ab
Eniamza <eniamza0@gmail.com>
2024-06-21 02:39:59 +0600

    Feat: ResetPassword Scaffold

--- src/app/api/(auth)/users/resetPassword/route.ts
+++ src/app/api/(auth)/users/resetPassword/route.ts
@@ -4,6 +4,28 @@ import { compare, hash } from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 import {singleResetMail} from "@/helpers/mailer";
+import exp from "constants";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function GET(request: NextRequest,response: NextResponse) {
+    try {
+
+        await dbConnect();
+
+
+        const token = request.nextUrl.searchParams.get('token');
+
+        console.log(token);
+
+        return NextResponse.json({
+            message: "If this email is registered, a password reset link will be sent to your email",
+        });
+        
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+        
+    }
+}
 
 export async function POST(request: NextRequest) {
 
@@ -56,7 +78,5 @@ try {
 } catch (error:any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
 }
-    
-    
-
 }
+
