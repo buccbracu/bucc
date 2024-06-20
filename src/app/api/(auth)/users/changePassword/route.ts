d652254816730263deb11e3053258502e96b1eb6
Eniamza <eniamza0@gmail.com>
2024-06-20 11:41:48 +0600

    Feat: Finished the Change Password Endpoint

--- src/app/api/(auth)/users/changePassword/route.ts
+++ src/app/api/(auth)/users/changePassword/route.ts
@@ -37,57 +37,72 @@ export async function GET(request: NextRequest) {
 
     await singleVerifyMail(user.user.name,user.user.email,verifyToken)
 
-
-
-
     return NextResponse.json({ message: "Verification Mail Sent"});
 
 }
 
 export async function POST(request: NextRequest) {
-    await dbConnect();
-    const user = {"user":{"name":"Tashfeen Azmaine","email":"tashfeen.azmaine@g.bracu.ac.bd","image":null,"id":"667323824c238b4f56027c0b","designation":"GENERAL MEMBER","buccDepartment":"RESEARCH AND DEVELOPMENT"},"expires":"2024-07-20T03:49:57.402Z"}
-
-    if (!user) {
-        return NextResponse.json({
-            message: "You are not authorized to initiate Password Change",
-        });
+    try {
+        await dbConnect();
+        const user = {"user":{"name":"Tashfeen Azmaine","email":"tashfeen.azmaine@g.bracu.ac.bd","image":null,"id":"667323824c238b4f56027c0b","designation":"GENERAL MEMBER","buccDepartment":"RESEARCH AND DEVELOPMENT"},"expires":"2024-07-20T03:49:57.402Z"}
+    
+        if (!user) {
+            return NextResponse.json({
+                message: "You are not authorized to initiate Password Change",
+            });
+        }
+    
+        const { oldPassword, newPassword, verifyToken } = await request.json();
+    
+        const member = await UserAuth.findOne({ _id: user.user.id })
+    
+        if (member.expiresIn < new Date()) {
+            return NextResponse.json({
+                message: "Verification Token has expired",
+            });
+        }
+    
+        if (member.verifyToken !== verifyToken) {
+            return NextResponse.json({
+                message: "Verification Token is invalid",
+            });
+        }
+    
+        const isSameOldPassword = await compare(oldPassword, member.password);
+    
+        if (!isSameOldPassword) {
+            return NextResponse.json({
+                message: "Old Password is incorrect",
+            });
+        }
+    
+        const isSameNewPassword = await compare(newPassword, member.password);
+    
+        if (isSameNewPassword) {
+            return NextResponse.json({
+                message: "New Password cannot be the same as the old password",
+            });
+        }
+    
+        const hashPass = await hash(newPassword, 10);
+    
+    
+        if (!member) {
+            return NextResponse.json({
+                message: "User not found",
+            });
+        }
+    
+        await member.findOneAndUpdate(
+                { _id: user.user.id },
+                { password: hashPass, verifyToken: null, expiresIn: null },
+                { new: true }
+            );
+    
+    
+        return NextResponse.json({ message: "Password Changed Successfully"});
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message });
+        
     }
-
-    const { oldPassword, newPassword } = await request.json();
-
-
-
-    const member = await UserAuth.findOne({ _id: user.user.id })
-
-    const isSameOldPassword = await compare(oldPassword, member.password);
-
-    if (!isSameOldPassword) {
-        return NextResponse.json({
-            message: "Old Password is incorrect",
-        });
-    }
-
-    const isSameNewPassword = await compare(newPassword, member.password);
-
-    if (isSameNewPassword) {
-        return NextResponse.json({
-            message: "New Password cannot be the same as the old password",
-        });
-    }
-
-    const hashPass = await hash(newPassword, 10);
-
-    //SEND A VERIFICATION MAIL HERE
-    //FIGURE OUT HOW TO PROCEED WITH THE VERIFICATION TOKEN
-
-    if (!member) {
-        return NextResponse.json({
-            message: "User not found",
-        });
-    }
-
-
-
-    return NextResponse.json({ user: member });
 }
