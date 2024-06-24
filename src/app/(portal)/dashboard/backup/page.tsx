cd00b1e888f0a5e47f09b6041d34222553d1cb7d
abir <abir4dev@gmail.com>
2024-06-25 02:52:42 +0600

    feat: Download CSV files

--- src/app/(portal)/dashboard/backup/page.tsx
+++ src/app/(portal)/dashboard/backup/page.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Heading from "@/components/portal/heading";
 import { Button } from "@/components/ui/button";
+import exportFromJSON from "export-from-json";
 
 export default function page() {
+  const fileName = "PreRegMemberInfos";
+  const exportType = exportFromJSON.types.csv;
+
   return (
     <main>
       <Heading
@@ -9,6 +14,18 @@ export default function page() {
         subHeadingText="This is the backup page. You can download the data from this page clicking the button below. The data will be downloaded in a zip file."
       />
       <Button>Download Data</Button>
+      <Button
+        className="mx-3"
+        onClick={() => {
+          fetch("http://localhost:3000/api/preregmembers")
+            .then((res) => res.json())
+            .then((response) =>
+              exportFromJSON({ data: response.users, fileName, exportType })
+            );
+        }}
+      >
+        Download PreRegMemberInfos CSV
+      </Button>
     </main>
   );
 }
