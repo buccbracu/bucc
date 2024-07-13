70aa17f52295d26047f1dd32e9cd65faf456c2b6
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-07-13 06:31:59 +0600

    FIX: 404 error in home "Learn More" button

---
+++ src/app/(main)/(pages)/about/about-us/page.tsx
@@ -0,0 +1,12 @@
+import UnderConstruction from '@/components/ui/under-construction'
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className="ext-3xl font-bold flex justify-center items-center min-h-[calc(100vh-140px)]">
+      <UnderConstruction />
+    </div>
+  )
+}
+
+export default AboutUs
