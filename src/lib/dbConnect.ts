237cf31b5fa8a5d120172804a8bb83b78106a78a
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2024-08-19 23:32:09 +0600

    Feat: PreRegMembers page created

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-require("dotenv").config();
+
+dotenv.config();
 
 type ConnectionObject = {
   isConnected?: number;
@@ -14,19 +16,34 @@ async function dbConnect(): Promise<void> {
     return;
   }
 
+  if (!MONGODB_URI || !MONGODB_DB) {
+    console.error(
+      "MONGODB_URI and MONGODB_DB must be defined in environment variables.",
+    );
+    process.exit(1);
+  }
+
   try {
-    const db = await mongoose.connect(MONGODB_URI!, {
+    const db = await mongoose.connect(MONGODB_URI, {
       dbName: MONGODB_DB,
       bufferCommands: false,
+      maxPoolSize: 500,
     });
 
     connection.isConnected = db.connections[0].readyState;
 
     console.log("New connection created");
   } catch (error) {
-    console.log("Error connecting to database", error);
+    console.error("Error connecting to database", error);
     process.exit(1);
   }
 }
+
+// Ensure graceful shutdown
+process.on("SIGINT", async () => {
+  await mongoose.disconnect();
+  console.log("Mongoose connection disconnected due to app termination");
+  process.exit(0);
+});
 
 export default dbConnect;
