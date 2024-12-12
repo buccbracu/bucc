39841ae5042675454c46db782df2c2c020555ee3
aaditx23 <adit.pranto@gmail.com>
2024-12-13 02:41:29 +0600

    Fix: firebase service key

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,10 +1,13 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = require("@/lib/firebase/firebase-service-key.json");
+  // Parse the service key from the environment variable
+  const serviceAccount = JSON.parse(`${process.env.NEXT_PUBLIC_FIREBASE_SERVICE_KEY}`);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
 export default admin;
+
