5d950811805fba382783407f7b2164022bdc7d7d
aaditx23 <adit.pranto@gmail.com>
2024-12-13 10:44:44 +0600

    Fix: Service key not found

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -2,8 +2,8 @@ import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   // Parse the service key from the environment variable
-  const serviceAccount = JSON.parse(`${process.env.NEXT_PUBLIC_FIREBASE_SERVICE_KEY}`);
-
+  const key = process.env.NEXT_PUBLIC_FIREBASE_SERVICE_KEY as string
+  const serviceAccount = JSON.parse(key);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
