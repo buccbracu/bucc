1e5512b5a03ced436c3823f93de0636b609552fc
aaditx23 <adit.pranto@gmail.com>
2024-12-14 21:14:01 +0600

    Feat: Added task overview endpoint

--- src/lib/firebase/notification.ts
+++ src/lib/firebase/notification.ts
@@ -12,6 +12,15 @@ interface NotificationPayload {
  * 
  * @param {NotificationPayload} notification - The notification details.
  * @returns {Promise<any>} - The response from Firebase Admin SDK after sending the notification.
+ * 
+ * topics = [panel_task, se_task, new_blog, edit_profile]
+ * body format:
+ *  task -> topic, fromDept, fromDesignation, toDept, toDesignation, description[0..100], deadline
+ *  new_blog -> topic, title, author_name, category
+ *  edit_profile -> topic
+ * 
+ * 
+ * 
  */
 export async function sendTopicNotification({
     title,
