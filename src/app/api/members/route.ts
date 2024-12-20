bcd531221cf93f1663d5da1e7d5dba3a27305930
aaditx23 <adit.pranto@gmail.com>
2024-12-20 17:53:13 +0600

    Fix: Se member permission

--- src/app/api/members/route.ts
+++ src/app/api/members/route.ts
@@ -37,7 +37,7 @@ export async function GET() {
     // Set fields to fetch based on designation
     const selectFields =
       user?.user.designation === "Senior Executive"
-        ? "email contactNumber emergencyContact name designation buccDepartment memberSocials profileImage"
+        ? "email contactNumber emergencyContact name designation buccDepartment profileImage"
         : "";
 
     // Query members with appropriate fields
