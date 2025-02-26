442898b79602929807aefd5aa28e1622d185c795
Angkon-Dutta-Joy <125572441+Angkon-Dutta-Joy@users.noreply.github.com>
2025-02-27 00:30:55 +0600

    Update buccInfo.ts
    updated the linkedin link

--- src/constants/buccInfo.ts
+++ src/constants/buccInfo.ts
@@ -9,7 +9,7 @@ const intakeInfo = {
 
 const buccSocials = {
   facebook: "https://www.facebook.com/BRACUCC",
-  linkedin: "https://www.linkedin.com/company/bucc",
+  linkedin: "https://www.linkedin.com/company/bracucc/",
   instagram: "https://www.instagram.com/bracucc",
   youtube: "https://www.youtube.com/@bracucc",
   github: "https://www.github.com/buccbracu",
