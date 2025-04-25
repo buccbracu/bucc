18fc7cce31bbdf38878424ed04c872c59e54e52f
abir <abir4dev@gmail.com>
2025-04-26 00:13:50 +0600

    feat: added changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/lib/env.mjs";
 
 export default {
-  schema: "./lib/db/schema",
+  schema: "./src/lib/db/schema",
   dialect: "postgresql",
   out: "./lib/db/migrations",
   dbCredentials: {
