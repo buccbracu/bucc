fafa20e1139a26c48e5609d2a4d9c60ec2d65d85
abir <abir4dev@gmail.com>
2025-04-25 20:14:08 +0600

    feat: added chat funtionality

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,85 @@
+import { createResource } from "@/lib/actions/resources";
+import { findRelevantContent } from "@/lib/ai/embedding";
+import { openai } from "@ai-sdk/openai";
+import { generateObject, streamText, tool } from "ai";
+import { z } from "zod";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    maxSteps: 3,
+    model: openai("gpt-4o-mini"),
+    messages,
+    system: ``,
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe("the content or resource to add to the knowledge base"),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+      getInformation: tool({
+        description: `get information from your knowledge base to answer questions.`,
+        parameters: z.object({
+          question: z.string().describe("the users question"),
+          similarQuestions: z.array(z.string()).describe("keywords to search"),
+        }),
+        execute: async ({ similarQuestions }) => {
+          console.log("===========111111111111============");
+          const results = await Promise.all(
+            similarQuestions.map(
+              async (question) => await findRelevantContent(question)
+            )
+          );
+          console.log("===========2222222222222222============");
+          // Flatten the array of arrays and remove duplicates based on 'name'
+          const uniqueResults = Array.from(
+            new Map(results.flat().map((item) => [item?.name, item])).values()
+          );
+          console.log("===========3333333333333============");
+          console.log(uniqueResults);
+          return uniqueResults;
+        },
+      }),
+      understandQuery: tool({
+        description: `understand the users query. use this tool on every prompt.`,
+        parameters: z.object({
+          query: z.string().describe("the users query"),
+          toolsToCallInOrder: z
+            .array(z.string())
+            .describe(
+              "these are the tools you need to call in the order necessary to respond to the users query"
+            ),
+        }),
+        execute: async ({ query }) => {
+          const { object } = await generateObject({
+            model: openai("gpt-4o-mini"),
+            system:
+              "You are a query understanding assistant. Analyze the user query and generate similar questions.",
+            schema: z.object({
+              questions: z
+                .array(z.string())
+                .max(3)
+                .describe("similar questions to the user's query. be concise."),
+            }),
+            prompt: `Analyze this query: "${query}". Provide the following:
+                    3 similar questions that could help answer the user's query`,
+          });
+          return object.questions;
+        },
+      }),
+    },
+  });
+  for await (const part of result.textStream) {
+    console.log(part);
+  }
+  return result.toDataStreamResponse();
+}
