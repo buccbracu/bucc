8613640ec1453127c79ecc6d9f5ef708ebfa6235
abir <abir4dev@gmail.com>
2025-04-30 01:27:46 +0600

    feat: update trasporter

--- src/app/api/(auth)/users/test/route.ts
+++ src/app/api/(auth)/users/test/route.ts
@@ -1,10 +1,8 @@
-
 import { welcomeMail } from "@/helpers/mailTemplates";
 import nodemailer from "nodemailer";
+import { NextRequest, NextResponse } from "next/server";
 
-// import MemberEBAssessment from "@/model/MemberEBAssessment";
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
@@ -12,23 +10,27 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USERNAME,
     pass: process.env.GMAIL_APP_SECRET,
   },
+  pool: true,
+  maxConnections: 5,
+  maxMessages: Infinity,
+  rateLimit: 5,
 });
-import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
     for (let i = 0; i < 160; i++) {
-    const mailOptions = {
-      from: process.env.GMAIL_USERNAME,
-      to: "tashfeen.azmaine@g.bracu.ac.bd",
-      subject: "Welcome to BUCC Portal",
-      text: welcomeMail(
-        "John Doe",
-        "tashfeen.azmaine@g.bracu.ac.bd",
-        "Iwonttellyoupass",
-      ),
-    };
-        await transporter.sendMail(mailOptions);
+      const mailOptions = {
+        from: process.env.GMAIL_USERNAME,
+        to: "tashfeen.azmaine@g.bracu.ac.bd",
+        subject: "Welcome to BUCC Portal",
+        text: welcomeMail(
+          "John Doe",
+          "tashfeen.azmaine@g.bracu.ac.bd",
+          "Iwonttellyoupass",
+        ),
+      };
+
+      await transporter.sendMail(mailOptions);
     }
 
     return NextResponse.json(
