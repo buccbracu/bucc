6573900156747be6742decfc18801bac490dae86
Shanid Sajjatuz Islam <shanidsajjatuz@gmail.com>
2025-11-15 04:17:06 +0600

    feat: migrate from PostgreSQL to MongoDB

--- src/lib/db/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./events";
-export * from "./eventBanners";
-export * from "./eventGalleries";
-export * from "./gallerySettings";
-export * from "./resources";
-export * from "./embeddings";
